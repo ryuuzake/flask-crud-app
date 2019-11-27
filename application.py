211a11ff26253c1435f3cc2badccdbb1c34b7c35
Riza Nafis <riza.nafis@gmail.com>
2019-11-27 10:21:20 +0700

    add requirements.txt

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ from flask import request
 from flask_sqlalchemy import SQLAlchemy
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
-database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "database.db"))
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
